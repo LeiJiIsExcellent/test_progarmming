96ead2c4cf061ce6ae999fa20843268076fb3b20
ChenJiaWei <b23042010@njupt.edu.cn>
2024-05-13 21:59:57 +0800

    test for daily

---
+++ 1.test.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: 1.test.cpp
+	> Author:cjw 
+	> Mail:1293632174@qq.com 
+	> Created Time: Mon 13 May 2024 07:32:59 PM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main() {
+    return 0;
+}
+
